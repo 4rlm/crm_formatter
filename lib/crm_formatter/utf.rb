bf44772618aa5e504444f3bc473fb8dce63389fc
Adam Booth <4rlm@protonmail.ch>
2018-05-18 17:10:18 -0500

    refactored utf csv perse.

--- lib/crm_formatter/utf.rb
+++ lib/crm_formatter/utf.rb
@@ -4,6 +4,7 @@ module CrmFormatter
   class UTF
 
     ## Accepted arg keys & values: either :file_path, :array_of_strings, or :array_of_hashes.
+    ## First entry point for UTF, then distributes to 3 diff meth options based on arg name and type.
     def validate_data(arg)
       ## Only accepts 1 arg, even though arg.keys / arg.values being used!
       err_msg = 'Error: Empty args sent to validate_data!'
@@ -12,7 +13,6 @@ module CrmFormatter
       arg_key = arg.keys.first
       arg_val = arg.values.first
 
-      binding.pry
       if arg_key == :file_path
         utf_results = validate_csv(arg_val)
       elsif arg_key == :array_of_hashes
@@ -30,36 +30,20 @@ module CrmFormatter
 
 
     def validate_csv(file_path)
-      counter = 0
-      inval_rows = []
-      val_hshs = []
-      headers = []
-      rows = []
-
+      headers, val_hshs, inval_rows = [], [], []
       File.open(file_path).each do |line|
         begin
-          # line_1 = line&.gsub(/\s/, ' ')&.strip ## Removes carriage returns and new lines.
-          # line_2 = force_utf_encoding(line_1) ## Removes non-utf chars.
           line = force_utf_encoding(line)
-
           CSV.parse(line) do |row|
-            row = row.collect { |x| x.try(:strip) } ## Strips white space from each el in row array.
-            if counter > 0
-              val_hshs << row_to_hsh(row, headers)
-              rows << row
-            else
-              headers = row
-            end
-            counter += 1
+            headers.empty? ? headers = row : val_hshs << row_to_hsh(row, headers)
           end
         rescue => er
+          counter = val_hshs.count + inval_rows.count
           inval_rows << {"#{counter}": "#{er.message}"}
-          counter += 1
           next
         end
       end
       utf_results = tally_results(inval_rows, val_hshs)
-      # utf_results = { error_report: tally_results(inval_rows, val_hshs), valid_csv_hashes: val_hshs }
     end
 
     def tally_results(inval_rows, val_hshs)
@@ -73,11 +57,6 @@ module CrmFormatter
       h.symbolize_keys
     end
 
-    # def row_to_hsh(row)
-    #   h = Hash[@headers.zip(row)]
-    #   h.symbolize_keys
-    # end
-
     def val_hsh(cols, hsh)
       keys = hsh.keys
       keys.each { |key| hsh.delete(key) if !cols.include?(key) }
@@ -85,26 +64,31 @@ module CrmFormatter
     end
 
 
+    ########################################
+    ### UTF ENCODING! ###
     def force_utf_encoding(text)
       text = text&.gsub(/\s/, ' ')&.strip ## Removes carriage returns and new lines.
       clean_text = text.delete("^\u{0000}-\u{007F}")
       clean_text = clean_text.gsub(/\s+/, ' ')
+      clean_text.squeeze!(" ")
+      clean_text.strip!
       clean_text
     end
 
 
+
+
+
+
     def validate_arrays(orig_arrays)
+      headers, val_hshs, inval_rows = [], [], []
       ## Not created yet.
       binding.pry
     end
 
 
     def validate_hashes(orig_hashes)
-      counter = 0
-      inval_rows = []
-      val_hshs = []
-      headers = []
-      rows = []
+      headers, val_hshs, inval_rows = [], [], []
 
       binding.pry
       orig_hashes.map do |hsh|
