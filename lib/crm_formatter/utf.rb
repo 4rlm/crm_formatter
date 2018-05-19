6f796fc26346effa84a59f25c3c6852ef8a4f0e8
Adam Booth <4rlm@protonmail.ch>
2018-05-19 10:32:36 -0500

    quite a bit of refactoring in UTF.  Still more to do.  Added a lot of notes of what to do next.

--- lib/crm_formatter/utf.rb
+++ lib/crm_formatter/utf.rb
@@ -4,10 +4,38 @@ module CrmFormatter
   class UTF
 
     def initialize
-      @utf_tls = []
-      @non_utf_texts = []
-      @row_id = 1
+      @final_results = [{ totals: {}, validations: {}, data_hashes: [] }]
+      @totals = {rows: 0, valid: 0, invalid: 0 }
+      @validations = { removed_non_utf8: 0, removed_carriage_returns: 0, non_utfs: [] }
+      @data_hashes = [] ## Send incoming data here, then just update it through here.
+      ## After completing processes, XXX method will be called, which will shovel 1 - @totals, 2 - @validations, 3 - @data_rows into @final_results
+      @row_id = 0
     end
+
+
+    def compile_results
+      binding.pry
+      @final_results[:totals] = @totals
+      @final_results[:validations] = @validations
+      @final_results[:data_hashes] = @data_hashes
+      binding.pry
+
+      ### If all saved properly:
+      ### 1. Try to query over the data.
+      ### 2. Then return @final_results, so it can be accessed by web_wrap, adr_wrap, or phone_wrap.
+      ### 3. Zero out totals: {}, validations: {}, data_hashes: [] so they can be used again without needing to initialize again.
+
+      ###### PROBABLY DON'T NEED THESE BELOW.  WAIT TILL LATER TO SEE.  ######
+      # utf_report = { valid: val_hshs.count,
+      #   invalid: inval_rows.length,
+      #   valid_details: make_groups_from_array(@encoding_report),
+      #   invalid_details: inval_rows.each_with_index { |hsh, i| "#{i+1}) Row #{hsh.keys[0]}: #{hsh.values[0]}." }
+      # }
+      # utf_results = { utf_report: utf_report, valid_csv_hashes: val_hshs }
+
+      return @final_results
+    end
+
 
     ## Accepted arg keys & values: either :file_path, :array_of_strings, or :array_of_hashes.
     ## First entry point for UTF, then distributes to 3 diff meth options based on arg name and type.
@@ -21,10 +49,16 @@ module CrmFormatter
 
       if arg_key == :file_path
         utf_results = validate_csv(arg_val)
+        binding.pry
+        # compile_results
       elsif arg_key == :array_of_hashes
         utf_results = validate_hashes(arg_val)
+        binding.pry
+        # compile_results
       elsif arg_key == :array_of_strings
         utf_results = validate_arrays(arg_val)
+        binding.pry
+        # compile_results
       else
         utf_results = {}
       end
@@ -99,10 +133,11 @@ module CrmFormatter
 
       puts "dif_count: #{dif_count}"
       puts "counter: #{counter}"
-      puts "@utf_tls: #{@utf_tls.count}"
+      puts "@validations: #{@validations.count}"
       binding.pry
 
-      # utf_results = tally_results(inval_rows, val_hshs)
+      # utf_results = compile_results(inval_rows, val_hshs)
+      compile_results ## Calls method to handle returns.
     end
 
 
@@ -116,8 +151,34 @@ module CrmFormatter
           binding.pry if !line.valid_encoding?
 
           CSV.parse(line) do |row|
-            headers.empty? ? headers = row : val_hshs << row_to_hsh(row, headers)
+            # headers.empty? ? headers = row : val_hshs << row_to_hsh(row, headers)
+
+
+            if headers.empty?
+              headers = row
+            else
+              @row_id +=1
+
+              data_hash = {row_id: @row_id }
+              parsed_hash = row_to_hsh(row, headers)  # Delete this if #3 below works.
+              binding.pry
+
+              data_hash = data_hash.merge(parsed_hash) ## 1) try this first.
+              # data_hash.merge!(parsed_hash) ## 2) try this if #1 works.
+              # data_hash.merge!(row_to_hsh(row, headers)) ## 3) try this if above works.  Then remove 'parsed_hash' above.
+
+              binding.pry
+              @data_hashes << data_hash
+              binding.pry
+              # val_hshs << row_to_hsh(row, headers)
+            end
+
+            # headers.empty? ? headers = row : @data_hashes << row_to_hsh(row, headers)
+
+
+
           end
+
         rescue => er
           binding.pry
           counter = val_hshs.count + inval_rows.count
@@ -127,71 +188,87 @@ module CrmFormatter
       end
 
       binding.pry
-      utf_results = tally_results(inval_rows, val_hshs)
+      ### utf_results will not be sent as large batch.  Will be sent to @data_hashes in real time.
+      # utf_results = compile_results(inval_rows, val_hshs)
+      compile_results ## Calls method to handle returns.
     end
+
+
+    # # getter
+    #     ['row_id', 'type_id', 'status'].each do |key|
+    #       define_method key do
+    #         data_hash[key]
+    #       end
+    #     end
+    #
+    #
+    # # setter
+    #     ['row_id', 'type_id', 'status'].each do |key|
+    #       define_method key do |val|
+    #         data_hash[key] = val
+    #       end
+    #     end
+    #     # data_hash.row_id
+    #     # data_hash.row_id = 'gh'
+
 
 
     ########################################
     ### UTF ENCODING! ###
 
     def validate_encoding(text)
+      # non_utf_texts <<<< !! REPLACED BY validations:
 
-     if !text.valid_encoding?
-       @non_utf_texts << text
-       removed_non_utf8 = text.chars.select(&:valid_encoding?).join
-       removed_non_utf8.gsub!('_', '')
-       removed_non_utf8.gsub!('ˌ', '') ### Keep this for future. If needed.
-       removed_non_utf8 = removed_non_utf8.delete("^\u{0000}-\u{007F}")
-       removed_carriage_returns = removed_non_utf8&.gsub(/\s+/, ' ')&.strip ## Removes carriage returns and new lines.
-       text = removed_carriage_returns  ## removed_non_utf8 is text.
-       @non_utf_texts << text
-     else
-       removed_non_utf8 = text.delete("^\u{0000}-\u{007F}")
-       removed_carriage_returns = removed_non_utf8&.gsub(/\s+/, ' ')&.strip ## Removes carriage returns and new lines.
+      if !text.valid_encoding?
+        @validations[:non_utfs] << {row_id: @row_id, non_utf_text: text}
+        @validations[:removed_non_utf8] +=1
+        text = text.chars.select(&:valid_encoding?).join
+        text.gsub!('_', '')
+        # text.gsub!('ˌ', '') ### Keep this for future. If needed.
+        text = text.delete("^\u{0000}-\u{007F}")
+      else
+        removed_non_utf8 = text.delete("^\u{0000}-\u{007F}")
+        if removed_non_utf8 != text
+          @validations[:non_utfs] << {row_id: @row_id, non_utf_text: text}
+          @validations[:removed_non_utf8] +=1
+          text = removed_non_utf8
+        end
+      end
 
-       utf_hsh = { text: text, removed_carriage_returns: removed_carriage_returns, removed_non_utf8: removed_non_utf8 }
-       diff = compare_diff(utf_hsh)
-       text = removed_carriage_returns
-     end
+      removed_carriage_returns = text&.gsub(/\s+/, ' ')&.strip ## Removes carriage returns and new lines.
+      if removed_carriage_returns != text
+        @validations[:removed_carriage_returns] +=1
+        text = removed_carriage_returns
+      end
 
-     @row_id +=1
      text
     end
     ########################################
 
 
-    def compare_diff(hsh)
-      res = []
-      hsh.to_a.reduce do |el, nxt|
-        res << nxt.first if el.last != nxt.last
-        el = nxt
-      end
-
-      res.compact!
-      @utf_tls += res
-    end
-
-
-    def tally_results(inval_rows, val_hshs)
-      binding.pry
-      ## Need to redo this!!
-
-      # utf_report = { valid: val_hshs.count,
-      #   invalid: inval_rows.length,
-      #   valid_details: make_groups_from_array(@utf_tls),
-      #   invalid_details: inval_rows.each_with_index { |hsh, i| "#{i+1}) Row #{hsh.keys[0]}: #{hsh.values[0]}." }
-      # }
-      # utf_results = { utf_report: utf_report, valid_csv_hashes: val_hshs }
-    end
+    # def compare_diff(hsh)
+    #   binding.pry
+    #   ### Need to redo this!
+    #
+    #   # res = []
+    #   # hsh.to_a.reduce do |el, nxt|
+    #   #   res << nxt.first if el.last != nxt.last
+    #   #   el = nxt
+    #   # end
+    #   #
+    #   # res.compact!
+    #   # @validations += res
+    # end
 
 
-    def make_groups_from_array(tally_arr)
-      ### Need to Redo This!!!
-
-      # hsh_by_grp = tally_arr.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
-      # puts "hsh_by_grp: #{hsh_by_grp}"
-      # hsh_by_grp
-    end
+    # def make_groups_from_array(tally_arr)
+    #   binding.pry
+    #   ### Need to Redo This!!!
+    #
+    #   # hsh_by_grp = tally_arr.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+    #   # puts "hsh_by_grp: #{hsh_by_grp}"
+    #   # hsh_by_grp
+    # end
 
 
 
@@ -201,6 +278,7 @@ module CrmFormatter
       headers, val_hshs, inval_rows = [], [], []
       ## Not created yet.
       binding.pry
+      compile_results ## Calls method to handle returns.
     end
 
 
